2349b8edf00e94f9f50fa758743238dd4d6a6e70
patterson-dtaylor <patterson.dtaylor@gmail.com>
2020-03-12 14:47:42 -0400

    portfolio-items built and updating sticky nav

--- js/index.js
+++ js/index.js
@@ -1,5 +1,20 @@
+window.onscroll = function() {stickyNav();};
+
+var navbar = document.getElementById("nav-bar");
+var sticky = navbar.offsetTop;
+
+function stickyNav() {
+    if (window.pageYOffset >= sticky) {
+        navbar.classList.add("sticky");
+    } else {
+        navbar.classList.remove("sticky");
+    }
+}
+
+// Nav-Open and Close
 const navToggle = document.querySelector('.nav-toggle');
 const navLinks = document.querySelectorAll('.nav__link');
+
 
 navToggle.addEventListener('click', () => {
     document.body.classList.toggle('nav-open');
